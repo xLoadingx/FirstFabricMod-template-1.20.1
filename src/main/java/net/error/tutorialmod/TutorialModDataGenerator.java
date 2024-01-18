38308595fd928cabd5bf5b98f411eaa564e57150
xLoadingx <Bellaandbeau32@gmail.com>
2024-01-17 19:06:04 -0500

    Initial commit

---
+++ src/main/java/net/error/tutorialmod/TutorialModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.error.tutorialmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TutorialModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
